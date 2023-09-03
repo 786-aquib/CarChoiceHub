a37bb5549de106ff3b2a1bddebdc8121d5d66162
Aquib Mahmood <aquibmahmood106@gmail.com>
2023-09-04 01:02:09 +0530

    Added Navbar Basically Added the Header Section Like Logo and SignIn button

--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,10 @@
 import Hero from "./Hero"
-
+import Navbar from "./Navbar"
+import Footer from "./Footer"
+import  CustomButton from "./CustomButton"
 export{
-    Hero
+    Hero,
+    Navbar,
+    Footer,
+    CustomButton
 }
